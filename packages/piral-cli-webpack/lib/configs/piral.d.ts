05474413409e39a111d1ef7253def709d9d8b393
Florian Rappl <florian.rappl@smapiot.com>
2022-02-03 18:13:31 +0100

    Removed unused files

--- packages/piral-cli-webpack/lib/configs/piral.d.ts
+++
@@ -1,2 +0,0 @@
-import * as webpack from 'webpack';
-export declare function getPiralConfig(template: string, dist: string, externals: Array<string>, develop?: boolean, sourceMaps?: boolean, contentHash?: boolean, minimize?: boolean, publicPath?: string, hmr?: number, progress?: boolean): Promise<webpack.Configuration>;
